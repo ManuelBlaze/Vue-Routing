c62093c6f6746bb2643cbba9dfec01b9614b49e4
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-01-31 18:04:36 -0500

    Adding routing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 
+import router from './routes';
+
 import App from './App.vue';
 
-const app = createApp(App)
-
-app.mount('#app');
+createApp(App)
+  .use(router)
+  .mount('#app');
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/teams',
+      component: TeamsList,
+    },
+    {
+      path: '/users',
+      component: UsersList,
+    },
+  ],
+});
