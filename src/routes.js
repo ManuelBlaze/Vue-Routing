c82809d13e81db9cfb2bff4bf80542cdedb28104
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-01-31 22:21:18 -0500

    Navigation & dynamic paths

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import TeamsList from './components/teams/TeamsList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 import UsersList from './components/users/UsersList.vue';
 
 export default createRouter({
@@ -13,6 +14,10 @@ export default createRouter({
     {
       path: '/users',
       component: UsersList,
+    },
+    {
+      path: '/teams/:teamId',
+      component: TeamMembers,
     },
   ],
   linkActiveClass: 'active-link',
